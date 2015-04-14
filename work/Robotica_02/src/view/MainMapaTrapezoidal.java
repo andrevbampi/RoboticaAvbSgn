ff33479ae79fe3f99729c893c3a2a6bceb4b6a38
andrevbampi <andrevbampi@gmail.com>
2015-04-14 02:43:42 -0300

    Criei a matriz de vértices, com os pontos médios e centro de trapézios, mas por algum motivo não está funcionando. Quase 3 da manhã, sem condições mentais de continuar. Continuaremos depois.

--- work/Robotica_02/src/view/MainMapaTrapezoidal.java
+++ work/Robotica_02/src/view/MainMapaTrapezoidal.java
@@ -8,6 +8,9 @@ import util.PontoMapa;
 public class MainMapaTrapezoidal {
 	private final int padrao = 99;
 	private final int obstaculo = -1;
+	//Números aleatórios da minha cabeça
+	private final int centro = 44;
+	private final int pontoMedio = 12;
 
 	private int[][] cenarioA = new int[6][7];
 	private int[][] cenarioB = new int[6][7];
@@ -19,6 +22,7 @@ public class MainMapaTrapezoidal {
 			new PontoMapa(4, 4), new PontoMapa(0, 5), new PontoMapa(2, 5),
 			new PontoMapa(3, 6), new PontoMapa(5, 6) };
 	private ArrayList<Trapezoide> listaTrapezoidesA = new ArrayList<Trapezoide>();
+	private int[][] pontosMediosA = new int[6][7];
 
 	private PontoMapa objetivoB = new PontoMapa(5, 6);
 	private PontoMapa inicioRoboB = new PontoMapa(0, 3);
@@ -26,6 +30,7 @@ public class MainMapaTrapezoidal {
 			new PontoMapa(3, 2), new PontoMapa(1, 3), new PontoMapa(4, 3),
 			new PontoMapa(2, 4), new PontoMapa(4, 5), new PontoMapa(3, 6) };
 	private ArrayList<Trapezoide> listaTrapezoidesB = new ArrayList<Trapezoide>();
+	private int[][] pontosMediosB = new int[6][7];
 
 
 	public void preencherCenarioInicial(char cenario) throws Exception {
@@ -97,6 +102,83 @@ public class MainMapaTrapezoidal {
 		}
 	}
 	
+	public void montarMatrizPontosMedios(char cenario) throws Exception {
+		switch(cenario) {
+			case 'A': montarMatrizPontosMedios(listaTrapezoidesA, cenarioA, pontosMediosA);
+					  break;
+			case 'B': montarMatrizPontosMedios(listaTrapezoidesB, cenarioB, pontosMediosB);
+		}
+	}
+	
+	private void montarMatrizPontosMedios(ArrayList<Trapezoide> listaTrapezoides,
+			                              int[][] cenario,
+			                              int[][] pontosMedios) throws Exception {
+		//Copiar a matriz de cenário
+		for (int x = 0; x < cenario.length; x++) {
+			for (int y = 0; y < cenario[x].length; y++) {
+				pontosMedios[x][y] = cenario [x][y];
+			}
+		}
+		
+		Trapezoide trapTemp = null;
+		for (Trapezoide trapezoide : listaTrapezoides) {
+			//O centro dos trapezóides também serão vértices. Não haverá distinção.
+			pontosMedios[trapezoide.getX()][trapezoide.getyCentro()] = centro;
+			for (Trapezoide trapezoide2 : listaTrapezoides) {
+				if (!trapezoide.equals(trapezoide2)) {
+					if (trapezoide.getX() == (trapezoide2.getX()-1)) {
+						trapTemp = new Trapezoide();
+						trapTemp.setX(trapezoide2.getX());
+						if (trapezoide.getyInicial() < trapezoide2.getyInicial()) {
+							trapTemp.setyInicial(trapezoide2.getyInicial());
+						} else {
+							trapTemp.setyInicial(trapezoide.getyInicial());
+						}
+						if (trapezoide.getyFinal() < trapezoide2.getyFinal()) {
+							trapTemp.setyFinal(trapezoide.getyFinal());
+						} else {
+							trapTemp.setyFinal(trapezoide2.getyFinal());
+						}
+						trapTemp.calcularCentro();
+						pontosMedios[trapTemp.getX()][trapTemp.getyCentro()] = pontoMedio;
+					}
+				}
+			}
+		}
+	}
+	
+	public String imprimirPontosMedios(char cenario) {
+		switch(cenario) {
+			case 'A': return imprimirPontosMedios(pontosMediosA);
+			case 'B': return imprimirPontosMedios(pontosMediosB);
+		}
+		return "";
+	}
+	
+	private String imprimirPontosMedios(int[][] pontosMedios) {
+ 		StringBuilder str = new StringBuilder();
+
+		for (int i = 0; i < pontosMedios[0].length; i++) {
+			for (int j = 0; j < pontosMedios.length; j++) {
+				if (pontosMedios[j][i] == pontoMedio)
+					str.append("[PM]");
+				else if (pontosMedios[j][i] == centro)
+					str.append("[ce]");
+				else if (pontosMedios[j][i] == obstaculo)
+					str.append("[XX]");
+				else {
+					String s = String.valueOf(pontosMedios[j][i]);
+					if (s.length() < 2)
+						str.append("[" + String.format("%1s", "0") + s + "]");
+					else
+						str.append("[" + s + "]");
+				}
+			}
+			str.append("\n");
+		}
+		return str.toString();
+	}
+	
 	public static void main(String[] args) {
 		try {
 			MainMapaTrapezoidal mapaTrapezoidal = new MainMapaTrapezoidal();
@@ -105,10 +187,18 @@ public class MainMapaTrapezoidal {
 			mapaTrapezoidal.preencherCenarioInicial('A');
 			mapaTrapezoidal.mostrarTrapezoides('A');
 			System.out.println("\n");
+			mapaTrapezoidal.montarMatrizPontosMedios('A');
+			System.out.println("\n");
+			mapaTrapezoidal.imprimirPontosMedios('A');
+			System.out.println("\n");
 			
 			System.out.println("Cenário B");
 			mapaTrapezoidal.preencherCenarioInicial('B');
 			mapaTrapezoidal.mostrarTrapezoides('B');
+			System.out.println("\n");
+			mapaTrapezoidal.montarMatrizPontosMedios('B');
+			System.out.println("\n");
+			mapaTrapezoidal.imprimirPontosMedios('B');
 			System.out.println("\n");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
