e30a042fe357ec2f97012dd09997cc30c307b62e
andrevbampi <andrevbampi@gmail.com>
2015-04-14 00:16:14 -0300

    Criando a classe Trapezoide

---
+++ work/Robotica_02/src/mapaTrapezoidal/Trapezoide.java
@@ -0,0 +1,41 @@
+package mapaTrapezoidal;
+
+public class Trapezoide {
+	private int x;
+	private int yInicial;
+	private int yFinal;
+	private final int padrao = 99;
+	
+	public Trapezoide() {
+		x = padrao;
+		yInicial = padrao;
+		yFinal = padrao;
+	}
+	
+	public boolean estaVazio() {
+		return (x==padrao) && (yInicial==padrao) && (yFinal==padrao);
+	}
+	
+	public boolean faltaFim() {
+		return (x<padrao) && (yInicial<padrao) && (yFinal==padrao);
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getyInicial() {
+		return yInicial;
+	}
+	public void setyInicial(int yInicial) {
+		this.yInicial = yInicial;
+	}
+	public int getyFinal() {
+		return yFinal;
+	}
+	public void setyFinal(int yFinal) {
+		this.yFinal = yFinal;
+	}
+}
